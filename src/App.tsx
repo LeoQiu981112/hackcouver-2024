749a6504c8336f87c6d31f24d5c2d300d1f00644
LeoQiu981112 <qwleo1112@gmail.com>
2023-12-07 13:58:32 -0800

    testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import * as React from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from './routes';
-import './index.css';
+import 'index.css';
 
 const App: React.FC = () => (
   <Router>
