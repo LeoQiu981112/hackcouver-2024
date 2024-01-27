b25f5670c57ccfd61b22062b6a2f3ce06a159319
LeoQiu981112 <qwleo1112@gmail.com>
2024-01-27 14:07:08 -0800

    test path

--- vite.config.ts
+++ vite.config.ts
@@ -4,8 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/hackcouver-2024/', // replace 'your-repo-name' with the name of your GitHub repository
   build: {
     assetsDir: 'assets',
   },
-
-})
+});
