f5a6cdb243f243e887d8e578103637c3486a9fa5
LeoQiu981112 <qwleo1112@gmail.com>
2024-01-27 14:56:07 -0800

    gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/hackcouver-2024/'
+  base: '/hackcouver-2024/',
   build: {
     assetsDir: 'assets',
   },
